a2f5560c44c83cbe8e75e52ed9f1f5bd285170bb
Julian Rivera <me@juarri.com>
2023-01-17 20:10:28 -0800

    app functionality done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import generatePassword from '../scripts/generatePassword';
+
+	let length = 12;
+
+	let hasLowercaseCharacters = true;
+	let hasUppercaseCharacters = true;
+	let hasNumbers = true;
+	let hasSymbols = true;
+
+	let minimumAmountOfNumbers = 0;
+	let minimumAmountOfSymbols = 0;
+
+	$: generatePasswordParameters = {
+		length,
+		hasLowercaseCharacters,
+		hasUppercaseCharacters,
+		hasNumbers,
+		hasSymbols,
+		minimumAmountOfNumbers,
+		minimumAmountOfSymbols
+	};
+
+	$: currentPassword = generatePassword(generatePasswordParameters);
+
+	const handleGenerate = () => {
+		currentPassword = generatePassword(generatePasswordParameters);
+	};
+</script>
+
+<h1>Key</h1>
+<p>Password generator</p>
+
+<p>{currentPassword}</p>
+<button on:click={handleGenerate}>Generate</button>
+
+<h2>Options</h2>
+
+<div>
+	<div>Length {length}</div>
+	<div><input type="range" bind:value={length} min={8} max={128} /></div>
+</div>
+
+<div>
+	<div>Lowercase Letters</div>
+	<div><input type="checkbox" bind:checked={hasLowercaseCharacters} /></div>
+</div>
+
+<div>
+	<div>Uppercase Letters</div>
+	<div><input type="checkbox" bind:checked={hasUppercaseCharacters} /></div>
+</div>
+
+<div>
+	<div>Numbers</div>
+	<div><input type="checkbox" bind:checked={hasNumbers} /></div>
+</div>
+
+<div>
+	<div>Symbols Letters</div>
+	<div><input type="checkbox" bind:checked={hasSymbols} /></div>
+</div>
+
+<div>
+	<div>Minimum numbers {minimumAmountOfNumbers}</div>
+	<div><input type="range" bind:value={minimumAmountOfNumbers} min={0} max={length / 2} /></div>
+</div>
+
+<div>
+	<div>Minimum symbols {minimumAmountOfSymbols}</div>
+	<div><input type="range" bind:value={minimumAmountOfSymbols} min={0} max={length / 2} /></div>
+</div>
---
+++ src/scripts/generatePassword.ts
@@ -0,0 +1,82 @@
+const shuffleString = (word: string) => {
+	const a = word.split('');
+	const n = word.length;
+
+	for (var i = n - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+	return a.join('');
+};
+
+const characterCodes = Array.from(Array(26)).map((_, i) => i + 97);
+export const lowercaseCharacters = characterCodes.map((code) => String.fromCharCode(code));
+export const uppercaseCharacters = lowercaseCharacters.map((letter) => letter.toUpperCase());
+
+export const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+export const symbols = ['!', '@', '#', '$', '%', '^', '&', '*'];
+
+type generatePasswordParameters = {
+	length?: number;
+	hasLowercaseCharacters?: boolean;
+	hasUppercaseCharacters?: boolean;
+	hasNumbers?: boolean;
+	hasSymbols?: boolean;
+	minimumAmountOfNumbers?: number;
+	minimumAmountOfSymbols?: number;
+};
+
+const generatePassword = ({
+	length = 12,
+	hasLowercaseCharacters = true,
+	hasUppercaseCharacters = true,
+	hasNumbers = true,
+	hasSymbols = false,
+	minimumAmountOfNumbers = 0,
+	minimumAmountOfSymbols = 0
+}: generatePasswordParameters) => {
+	let availableCharacters = [
+		...(hasLowercaseCharacters ? lowercaseCharacters : []),
+		...(hasUppercaseCharacters ? uppercaseCharacters : []),
+		...(hasNumbers ? numbers : []),
+		...(hasSymbols ? symbols : [])
+	];
+
+	let password = '';
+
+	if (availableCharacters.length === 0) return '';
+
+	if (length < minimumAmountOfNumbers + minimumAmountOfSymbols) return '';
+
+	if (hasNumbers) {
+		for (let i = 0; i < minimumAmountOfNumbers; i++) {
+			const randomIndex = Math.floor(Math.random() * numbers.length);
+			password += numbers[randomIndex];
+		}
+
+		length -= minimumAmountOfNumbers;
+	}
+
+	if (hasSymbols) {
+		for (let i = 0; i < minimumAmountOfSymbols; i++) {
+			const randomIndex = Math.floor(Math.random() * symbols.length);
+			password += symbols[randomIndex];
+		}
+
+		length -= minimumAmountOfSymbols;
+	}
+
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * availableCharacters.length);
+		password += availableCharacters[randomIndex];
+	}
+
+	const shuffledPassword = shuffleString(password);
+
+	return shuffledPassword;
+};
+
+export default generatePassword;
